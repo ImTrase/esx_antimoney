030f75c79b8d150565726a44bf1c46b285827164
TraseDeveloper <86205210+TraseDeveloper@users.noreply.github.com>
2022-05-26 02:29:40 -0400

    Added lua54 to fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 author 'Trase'
 description 'A fivem script (ESX) to stop modders from having to much money.'
+lua54 'yes'
 
 shared_script 'config.lua'
 server_scripts 'server/*.lua'
